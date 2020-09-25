349ac17890fabf8ae510434da83eac83ed35e4c2
agusescalante <agustinescalante27@gmail.com>
2020-09-25 02:58:07 -0300

    Closes #6 - Archivos creados de tests

---
+++ .bash_history
@@ -0,0 +1,35 @@
+ls 
+php artisan dusk
+php artisan dusk:make LoginTest
+sudo php artisan dusk:make LoginTest
+sudo php artisan dusk:make LoginTest
+php artisan dusk:make LoginTest
+php artisan dusk:make LoginTest
+pwd
+chmod -R 777 storage/
+su -chmod -R 777 storage/
+sudo -chmod -R 777 storage/
+-chmod  777 storage/
+chmod  777 storage/
+php artisan dusk:make LoginTest
+php artisan dusk:make LoginTest
+exit
+php artisan dusk
+exit
+php artisan dusk
+php artisan dusk
+php artisan dusk
+php artisan dusk
+php artisan migrate
+php artisan dusk
+php artisan dusk --filter=RegisterLogin
+php artisan dusk --filter=RegisterLogin
+php artisan dusk 
+php artisan dusk --filter=RegisterLogin
+php artisan dusk --filter=RegisterLogin
+php artisan dusk 
+php artisan dusk 
+php artisan migrate
+php artisan dusk 
+php artisan migrate
+exit
---
+++ tests/Browser/RegisterLogin.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class RegisterLogin extends DuskTestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testRegister()
+    {
+        
+
+        $user = User::factory()->make([
+            'email'=>'test@laravel.com',
+            'password'=>bcrypt('123456789')        
+            ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/register')
+                    ->assertSee('Laravel')
+                    ->type('name' ,$user->name)
+                    ->type('email',$user->email)
+                    ->type('password','12345678')
+                    ->type('password_confirmation','12345678')
+                    ->press('REGISTER')
+                    ->assertSee('Dashboard');
+        });
+    }
+
+
+}
